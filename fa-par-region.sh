916a494905043f134459ddd4a74166e0c20f8847
Thomas Minier <tminier01@gmail.com>
2016-03-11 13:44:25 +0100

    add ful setup for Fedra testing

--- fa-par-region.sh
+++ fa-par-region.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-./cli.sh -verbose 3 -d examples/FaParRegion.ttl @q examples/q-erdf-1.txt > exec.txt
+#bash cli.sh -verbose 3 -d examples/FaParRegion.ttl @q examples/q-erdf-1.txt > exec.txt
+bash cli.sh -verbose 3 -c examples/FaParRegionSetup/fa-par-region-config.prop @q examples/q-erdf-1.txt > exec.txt
