0451c8d97637bb900a00ca1af452be12fd3438f0
Thomas Minier <tminier01@gmail.com>
2016-05-02 13:05:08 +0200

    Update getIRNCFederation.sh

--- scripts/getIRNCFederation.sh
+++ scripts/getIRNCFederation.sh
@@ -16,8 +16,16 @@ for i in  `seq $firstPort $lastPort`; do
    y=${line#* }
    ir=$(($ir+$x))
    nc=$(($nc+$y))
-   irc_tot="${irc_tot} ${x}"
-   nc_tot="${nc_tot} ${y}"
+   if [[ -z "${irc_tot}" ]]; then
+       irc_tot="${x} 0"
+   else
+       irc_tot="${x} ${irc_tot}"
+   fi
+   if [[ -z "${nc_tot}" ]]; then
+       nc_tot="${y} 0"
+   else
+       nc_tot="${y} ${nc_tot}"
+   fi
 done
 
 if [ -f "${prefix}PublicEndpoint" ]; then
