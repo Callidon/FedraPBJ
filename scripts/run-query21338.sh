c16d8fe56436e1ed9a9f804e0dcd7aa8529ff2be
Thomas Minier <tminier01@gmail.com>
2016-05-02 14:51:17 +0200

    Create run-query21338.sh

---
+++ scripts/run-query21338.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# WARNING
+# launch federation for diseasome & proxies before
+./cli.sh -c /home/fedra/fedra/data/diseasomeSetup/confFileDiseasome -d /home/fedra/fedra/data/diseasomeSetup/federation_bis.ttl @q /home/fedra/fedra/data/diseasomeSetup/query21338
