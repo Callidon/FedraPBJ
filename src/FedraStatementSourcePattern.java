267cf4289ec6b8f0503ff0dd582d821e6966a5ed
Thomas Minier <tminier01@gmail.com>
2016-05-03 16:49:17 +0200

    Update FedraStatementSourcePattern.java

--- src/FedraStatementSourcePattern.java
+++ src/FedraStatementSourcePattern.java
@@ -4,7 +4,9 @@ import com.fluidops.fedx.structures.QueryInfo;
 import org.openrdf.query.algebra.StatementPattern;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Statement source pattern implementing a set of set of sources
@@ -12,17 +14,30 @@ import java.util.List;
 public class FedraStatementSourcePattern extends StatementSourcePattern {
 
     private List<List<StatementSource>> relevantSources;
+    private Map<StatementSource, StatementSourcePattern> relevantSourcePatterns;
 
     public FedraStatementSourcePattern(StatementPattern node, QueryInfo queryInfo) {
         super(node, queryInfo);
         relevantSources = new ArrayList<>();
+        relevantSourcePatterns = new HashMap<>();
     }
 
     public List<List<StatementSource>> getRelevantSources() {
         return relevantSources;
     }
 
-    public void setRelevantSources(List<List<StatementSource>> relevantSources) {
-        this.relevantSources = relevantSources;
+    public Map<StatementSource, StatementSourcePattern> getRelevantSourcePatterns() {
+        return relevantSourcePatterns;
+    }
+
+    public void setRelevantSources(StatementPattern stmt, List<List<StatementSource>> relevantSources, QueryInfo queryInfo) {
+        for(List<StatementSource> endpoints : relevantSources) {
+            this.relevantSources.add(new ArrayList<>(endpoints));
+            for(StatementSource source : endpoints) {
+                StatementSourcePattern stmtSource = new StatementSourcePattern(stmt, queryInfo);
+                stmtSource.addStatementSource(source);
+                this.relevantSourcePatterns.put(source, stmtSource);
+            }
+        }
     }
 }
