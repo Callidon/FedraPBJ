d00539c559ac871625f4e42451347732336d9dec
Thomas Minier <tminier01@gmail.com>
2016-05-03 16:50:18 +0200

    Update upload-src.sh

--- scripts/upload-src.sh
+++ scripts/upload-src.sh
@@ -4,5 +4,8 @@
 scp src/com/fluidops/fedx/evaluation/join/ControlledWorkerBoundJoin.java src/com/fluidops/fedx/evaluation/join/ParallelFedraPartitioning.java \
     fedra@172.16.9.3:fedXModif_2016/src/com/fluidops/fedx/evaluation/join/
 
-scp src/com/fluidops/fedx/optimizer/FedraSourceSelection.java \
+scp src/com/fluidops/fedx/optimizer/FedraSourceSelection.java src/com/fluidops/fedx/optimizer/SourceSelection.java \
     fedra@172.16.9.3:fedXModif_2016/src/com/fluidops/fedx/optimizer/
+
+scp src/com/fluidops/fedx/algebra/FedraStatementSourcePattern.java \
+    fedra@172.16.9.3:fedXModif_2016/src/com/fluidops/fedx/algebra/
