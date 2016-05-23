a6c1edc4725f5d97c9208f187d116245a04f6101
thomas Minier <tminier01@gmail.com>
2016-05-23 16:07:14 +0200

    add script for executing all the queries in parallel

--- scripts/parallel_execution.sh
+++ scripts/parallel_execution.sh
@@ -5,7 +5,7 @@
 DATA_PATH="/home/fedra/fedra/data/watDivSetup/"
 CLI_PATH="/home/fedra/fedXModif_2016/"
 CONFIG_FILE="${DATA_PATH}/confFileWatDiv" # /home/fedra/fedra/data/watDivSetup/confFileWatDiv
-FEDERATION_FILE="${DATA_PATH}/federation_online.ttl" # /home/fedra/fedra/data/watDivSetup/federation.ttl
+FEDERATION_FILE="${DATA_PATH}/federation_bis.ttl" # /home/fedra/fedra/data/watDivSetup/federation.ttl
 QUERIES_FILE="${DATA_PATH}/queries15_500_100_b"
 QUERIES_TO_EXECUTE_FILE="${DATA_PATH}/queriesToExecute_0_bkp"
 RESULTS_FOLDER="${DATA_PATH}/results"
@@ -13,11 +13,13 @@ RESULTS_FOLDER="${DATA_PATH}/results"
 # for each query
 while IFS='' read -r line || [[ -n "$line" ]]; do
   cd $DATA_PATH
-  # get query to execute
-  query=`sed "${line}q;d" $QUERIES_FILE`
+  # get query
+  queryFile=`mktemp`
+  sed "${line}q;d" $QUERIES_FILE > $queryFile
 
   # execute query
   cd $CLI_PATH
-  # resultFile="${RESULTS_FOLDER}/query${line}"
-  ./cli.sh -c $CONFIG_FILE -d $FEDERATION_FILE -f JSON -folder $RESULTS_FOLDER @q "${query}"
+  resultFile="${RESULTS_FOLDER}/query${line}"
+  nohup ./cli.sh -c $CONFIG_FILE -d $FEDERATION_FILE -f JSON -folder $RESULTS_FOLDER/answers @q $queryFile | grep -e "duration=" > ${resultFile} 2> ${resultFile}.err &
+  echo "query ${line} : pid = ${!}"
 done < "${QUERIES_TO_EXECUTE_FILE}"
