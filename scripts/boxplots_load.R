88c6245643fd055557a57430b2dd7aaf0e37d784
Thomas Minier <tminier01@gmail.com>
2016-06-03 11:20:45 +0200

    optimize R scripts to regroup graphs

--- scripts/boxplots_load.R
+++ scripts/boxplots_load.R
@@ -1,68 +1,18 @@
 #!/usr/bin/env Rscript
-# Script to produce boxplots for the results of the load balancing
+# Script to produce boxplots for the results of the load balancing by federation
 # author : Thomas Minier
-
-require(ggplot2)
-require(scales)
-
-# Function used to process a value from output tables
-processTable <- function(setupName, nbEndpoints, outputFedraPath, outputPBJHybridPath) {
-	# read datas from the files
-	outputFedra <- read.table(outputFedraPath)[13:(13 + nbEndpoints)]
-	outputPBJHybrid <- read.table(outputPBJHybridPath)[13:(12 + nbEndpoints)]
-
-	# set setup name column
-	outputEngine$dataset <- setupName
-	outputPBJHybrid$dataset <- setupName
-
-	# set approach column
-	outputEngine$approach <- approachFedxName
-	outputFedra$approach <- approachFedxFedraName
-	outputPBJPre$approach <- approachPBJPreName
-	outputPBJPost$approach <- approachPBJPostName
-	outputPBJHybrid$approach <- approachPBJHybridName
-
-	# concat the tables and return it
-	result <- rbind(outputEngine, outputFedra, outputPBJPre, outputPBJPost, outputPBJHybrid)
-	return(result)
-}
-
-# add log10 scale + labels on X and Y axis on a ggplot's boxplot
-bootstrap_ggplot <- function(plot, xLabel, yLabel, boxplotLabel = NULL, defaultScale=FALSE) {
-	p <- plot + xlab(xLabel) + ylab(yLabel)
-
-	if(! defaultScale) {
-		p <- p + scale_y_continuous(trans = log10_trans(), breaks = trans_breaks("log10", function(x) 100^x), labels = trans_format("log10", math_format(10^.x)))
-	}
-
-	if(is.null(boxplotLabel)) {
-		p <- p + geom_boxplot()
-	} else {
-		p <- p + geom_boxplot(boxplotLabel)
-	}
-	return(p)
-}
 
 # path to results files
 # with the classic federation
-outputWatDivEngine <- "../results/watDiv/outputFedXengineFEDERATION10Client"
 outputWatDivFedra <- "../results/watDiv/outputFedXFedraFEDERATION10Client"
-outputWatDivPBJPre <- "../results/watDiv/outputFedXFedra-PBJ-preFEDERATION10Client"
-outputWatDivPBJPost <- "../results/watDiv/outputFedXFedra-PBJ-postFEDERATION10Client"
 outputWatDivPBJHybrid <- "../results/watDiv/outputFedXFedra-PBJ-hybridFEDERATION10Client"
 
 # with a federation of 20 endpoints
-outputWatDiv20eEngine <- "../results/watDiv/outputFedXengineFEDERATION20Client"
 outputWatDiv20eFedra <- "../results/watDivMore/outputFedXFedraFEDERATION20Client"
-outputWatDiv20ePBJPre <- "../results/watDiv/outputFedXFedra-PBJ-preFEDERATION20Client"
-outputWatDiv20ePBJPost <- "../results/watDiv/outputFedXFedra-PBJ-postFEDERATION20Client"
 outputWatDiv20ePBJHybrid <- "../results/watDivMore/outputFedXFedra-PBJ-hybridFEDERATION20Client"
 
 # with a federation of 30 endpoints
-outputWatDiv30eEngine <- "../results/watDiv/outputFedXengineFEDERATION30Client"
 outputWatDiv30eFedra <- "../results/watDivMore/outputFedXFedraFEDERATION30Client"
-outputWatDiv30ePBJPre <- "../results/watDiv/outputFedXFedra-PBJ-preFEDERATION30Client"
-outputWatDiv30ePBJPost <- "../results/watDiv/outputFedXFedra-PBJ-postFEDERATION30Client"
 outputWatDiv30ePBJHybrid <- "../results/watDivMore/outputFedXFedra-PBJ-hybridFEDERATION30Client"
 
 # set the data frames
--- scripts/produce_boxplots.R
+++ scripts/produce_boxplots.R
@@ -20,6 +20,7 @@ approachPBJPostName <- "PBJ Post"
 approachPBJHybridName <- "PBJ Hybrid"
 
 execTimelabel <- "Execution time (s)"
+endpointsLabel <- "Number of endpoints in federation"
 tuplesLabel <- "Number of transferred tuples"
 completenessLabel <- "Completeness"
 strategyLabel <- "Strategy"
@@ -50,6 +51,37 @@ processTable <- function(setupName, value_ind, outputEnginePath, outputFedraPath
 
 	# concat the tables and return it
 	result <- rbind(outputEngine, outputFedra, outputPBJPre, outputPBJPost, outputPBJHybrid)
+	return(result)
+}
+
+# Function used to process a value from output tables with different federation sizes
+processFederationTables <- function(value_ind, outputFedraPath, outputPBJHybridPath, outputFedra20ePath, outputPBJHybrid20ePath, outputFedra30ePath, outputPBJHybrid30ePath) {
+	# read datas from the files
+	outputFedra <- read.table(outputFedraPath)[value_ind]
+	outputPBJHybrid <- read.table(outputPBJHybridPath)[value_ind]
+    outputFedra20e <- read.table(outputFedra20ePath)[value_ind]
+	outputPBJHybrid20e <- read.table(outputPBJHybrid20ePath)[value_ind]
+    outputFedra30e <- read.table(outputFedra30ePath)[value_ind]
+	outputPBJHybrid30e <- read.table(outputPBJHybrid30ePath)[value_ind]
+
+	# set setup name column
+    outputFedra$endpoints <- "10"
+	outputPBJHybrid$endpoints <- "10"
+    outputFedra20e$endpoints <- "20"
+	outputPBJHybrid20e$endpoints <- "20"
+    outputFedra30e$endpoints <- "30"
+	outputPBJHybrid30e$endpoints <- "30"
+
+	# set approach column
+	outputFedra$approach <- approachFedxFedraName
+	outputPBJHybrid$approach <- approachPBJHybridName
+    outputFedra20e$approach <- approachFedxFedraName
+	outputPBJHybrid20e$approach <- approachPBJHybridName
+    outputFedra30e$approach <- approachFedxFedraName
+	outputPBJHybrid30e$approach <- approachPBJHybridName
+
+	# concat the tables and return it
+	result <- rbind(outputFedra, outputPBJHybrid, outputFedra20e, outputPBJHybrid20e, outputFedra30e, outputPBJHybrid30e)
 	return(result)
 }
 
@@ -149,17 +181,11 @@ linkedMDBTable <- processTable(linkedMDBSetupName, 2, outputLinkedMDBEngine, out
 geoCoordinatesTable <- processTable(geoCoordinatesSetupName, 2, outputGeoCoordinatesEngine, outputGeoCoordinatesFedra, outputGeoCoordinatesPBJPre, outputGeoCoordinatesPBJPost, outputGeoCoordinatesPBJHybrid)
 SWDFTable <- processTable(swdfSetupName, 2, outputSWDFEngine, outputSWDFFedra, outputSWDFPBJPre, outputSWDFPBJPost, outputSWDFPBJHybrid)
 watDivTable <- processTable(watDivSetupName, 2, outputWatDivEngine, outputWatDivFedra, outputWatDivPBJPre, outputWatDivPBJPost, outputWatDivPBJHybrid)
-watDiv20eTable <- processTable(watDivSetupName, 2, outputWatDiv20eEngine, outputWatDiv20eFedra, outputWatDiv20ePBJPre, outputWatDiv20ePBJPost, outputWatDiv20ePBJHybrid)
-watDivPll20eTable <- processTable(watDivSetupName, 2, outputWatDivPll20eEngine, outputWatDivPll20eFedra, outputWatDivPll20ePBJPre, outputWatDivPll20ePBJPost, outputWatDivPll20ePBJHybrid)
-#watDiv30eTable <- processTable(watDivSetupName, 2, outputWatDiv30eEngine, outputWatDiv30eFedra, outputWatDiv30ePBJPre, outputWatDiv30ePBJPost, outputWatDiv30ePBJHybrid)
-#watDivPll30eTable <- processTable(watDivSetupName, 2, outputWatDivPll30eEngine, outputWatDivPll30eFedra, outputWatDivPll30ePBJPre, outputWatDivPll30ePBJPost, outputWatDivPll30ePBJHybrid)
-
-# TEMP - remove when all results are here
-watDiv30eTable <- processTable(watDivSetupName, 2, outputWatDivEngine, outputWatDiv30eFedra, outputWatDivPBJPre, outputWatDivPBJPost, outputWatDiv30ePBJHybrid)
-watDivPll30eTable <- processTable(watDivSetupName, 2, outputWatDivEngine, outputWatDivPll30eFedra, outputWatDivPBJPre, outputWatDivPBJPost, outputWatDivPll30ePBJHybrid)
-# END TEMP
-
 watDiv100Table <- processTable(watDiv100SetupName, 2, outputWatDiv100Engine, outputWatDiv100Fedra, outputWatDiv100PBJPre, outputWatDiv100PBJPost, outputWatDiv100PBJHybrid)
+
+watDivMoreTable <- processFederationTables(2, outputWatDivFedra, outputWatDivPBJHybrid, outputWatDiv20eFedra, outputWatDiv20ePBJHybrid, outputWatDiv30eFedra, outputWatDiv30ePBJHybrid)
+watDivMorePllTable <- processFederationTables(2, outputWatDivFedra, outputWatDivPBJHybrid, outputWatDivPll20eFedra, outputWatDivPll20ePBJHybrid, outputWatDivPll30eFedra, outputWatDivPll30ePBJHybrid)
+
 timesTable <- rbind(diseasomeTable, linkedMDBTable, geoCoordinatesTable, SWDFTable, watDivTable, watDiv100Table)
 
 # set the colnames
@@ -168,11 +194,9 @@ colnames(linkedMDBTable) <- c("time", "dataset", "Strategy")
 colnames(geoCoordinatesTable) <- c("time", "dataset", "Strategy")
 colnames(SWDFTable) <- c("time", "dataset", "Strategy")
 colnames(watDivTable) <- c("time", "dataset", "Strategy")
-colnames(watDiv20eTable) <- c("time", "dataset", "Strategy")
-colnames(watDiv30eTable) <- c("time", "dataset", "Strategy")
-colnames(watDivPll20eTable) <- c("time", "dataset", "Strategy")
-colnames(watDivPll30eTable) <- c("time", "dataset", "Strategy")
 colnames(watDiv100Table) <- c("time", "dataset", "Strategy")
+colnames(watDivMoreTable) <- c("time", "endpoints", "Strategy")
+colnames(watDivMorePllTable) <- c("time", "endpoints", "Strategy")
 colnames(timesTable) <- c("time", "dataset", "Strategy")
 
 # create the boxplots
@@ -200,25 +224,18 @@ pdf("../results/watDiv/execution_time.pdf", width=7, height=4)
 bootstrap_ggplot(ggplot(data = watDivTable, aes(x=Strategy, y=time)), strategyLabel, execTimelabel)
 dev.off()
 
-pdf("../results/watDivMore/execution_time_20endpoints.pdf", width=7, height=4)
-bootstrap_ggplot(ggplot(data = watDiv20eTable, aes(x=Strategy, y=time)), strategyLabel, execTimelabel)
-dev.off()
-
-pdf("../results/watDivMore/execution_time_30endpoints.pdf", width=7, height=4)
-bootstrap_ggplot(ggplot(data = watDiv30eTable, aes(x=Strategy, y=time)), strategyLabel, execTimelabel)
-dev.off()
-
-pdf("../results/watDivMore/parallelized/execution_time_20endpoints.pdf", width=7, height=4)
-bootstrap_ggplot(ggplot(data = watDivPll20eTable, aes(x=Strategy, y=time)), strategyLabel, execTimelabel)
-dev.off()
-
-pdf("../results/watDivMore/parallelized/execution_time_30endpoints.pdf", width=7, height=4)
-bootstrap_ggplot(ggplot(data = watDivPll30eTable, aes(x=Strategy, y=time)), strategyLabel, execTimelabel)
-dev.off()
-
 pdf("../results/watDiv100/execution_time.pdf", width=7, height=4)
 bootstrap_ggplot(ggplot(data = watDiv100Table, aes(x=Strategy, y=time)), strategyLabel, execTimelabel)
 dev.off()
+
+pdf("../results/watDivMore/execution_time.pdf", width=7, height=4)
+bootstrap_ggplot(ggplot(data = watDivMoreTable, aes(x=endpoints, y=time)), endpointsLabel, execTimelabel, aes(fill=Strategy))
+dev.off()
+
+pdf("../results/watDivMore/parallelized/execution_time.pdf", width=7, height=4)
+bootstrap_ggplot(ggplot(data = watDivMorePllTable, aes(x=endpoints, y=time)), endpointsLabel, execTimelabel, aes(fill=Strategy))
+dev.off()
+
 
 # For the number of transfered tuples
 # Process the datas & merge them into one unique table
@@ -227,17 +244,11 @@ linkedMDBTable <- processTable(linkedMDBSetupName, 11, outputLinkedMDBEngine, ou
 geoCoordinatesTable <- processTable(geoCoordinatesSetupName, 11, outputGeoCoordinatesEngine, outputGeoCoordinatesFedra, outputGeoCoordinatesPBJPre, outputGeoCoordinatesPBJPost, outputGeoCoordinatesPBJHybrid)
 SWDFTable <- processTable(swdfSetupName, 11, outputSWDFEngine, outputSWDFFedra, outputSWDFPBJPre, outputSWDFPBJPost, outputSWDFPBJHybrid)
 watDivTable <- processTable(watDivSetupName, 11, outputWatDivEngine, outputWatDivFedra, outputWatDivPBJPre, outputWatDivPBJPost, outputWatDivPBJHybrid)
-watDiv20eTable <- processTable(watDivSetupName, 11, outputWatDiv20eEngine, outputWatDiv20eFedra, outputWatDiv20ePBJPre, outputWatDiv20ePBJPost, outputWatDiv20ePBJHybrid)
-watDivPll20eTable <- processTable(watDivSetupName, 11, outputWatDivPll20eEngine, outputWatDivPll20eFedra, outputWatDivPll20ePBJPre, outputWatDivPll20ePBJPost, outputWatDivPll20ePBJHybrid)
-#watDiv30eTable <- processTable(watDivSetupName, 11, outputWatDiv30eEngine, outputWatDiv30eFedra, outputWatDiv30ePBJPre, outputWatDiv30ePBJPost, outputWatDiv30ePBJHybrid)
-#watDivPll30eTable <- processTable(watDivSetupName, 11, outputWatDivPll30eEngine, outputWatDivPll30eFedra, outputWatDivPll30ePBJPre, outputWatDivPll30ePBJPost, outputWatDivPll30ePBJHybrid)
-
-# TEMP - remove when all the results are here
-watDiv30eTable <- processTable(watDivSetupName, 11, outputWatDivEngine, outputWatDiv30eFedra, outputWatDivPBJPre, outputWatDivPBJPost, outputWatDiv30ePBJHybrid)
-watDivPll30eTable <- processTable(watDivSetupName, 11, outputWatDivEngine, outputWatDivPll30eFedra, outputWatDivPBJPre, outputWatDivPBJPost, outputWatDivPll30ePBJHybrid)
-# END TEMP
-
 watDiv100Table <- processTable(watDiv100SetupName, 11, outputWatDiv100Engine, outputWatDiv100Fedra, outputWatDiv100PBJPre, outputWatDiv100PBJPost, outputWatDiv100PBJHybrid)
+
+watDivMoreTable <- processFederationTables(11, outputWatDivFedra, outputWatDivPBJHybrid, outputWatDiv20eFedra, outputWatDiv20ePBJHybrid, outputWatDiv30eFedra, outputWatDiv30ePBJHybrid)
+watDivMorePllTable <- processFederationTables(11, outputWatDivFedra, outputWatDivPBJHybrid, outputWatDivPll20eFedra, outputWatDivPll20ePBJHybrid, outputWatDivPll30eFedra, outputWatDivPll30ePBJHybrid)
+
 tuplesTable <- rbind(diseasomeTable, linkedMDBTable, geoCoordinatesTable, SWDFTable, watDivTable, watDiv100Table)
 
 # set the colnames
@@ -246,11 +257,9 @@ colnames(linkedMDBTable) <- c("tuples", "dataset", "Strategy")
 colnames(geoCoordinatesTable) <- c("tuples", "dataset", "Strategy")
 colnames(SWDFTable) <- c("tuples", "dataset", "Strategy")
 colnames(watDivTable) <- c("tuples", "dataset", "Strategy")
-colnames(watDiv20eTable) <- c("tuples", "dataset", "Strategy")
-colnames(watDiv30eTable) <- c("tuples", "dataset", "Strategy")
-colnames(watDivPll20eTable) <- c("tuples", "dataset", "Strategy")
-colnames(watDivPll30eTable) <- c("tuples", "dataset", "Strategy")
 colnames(watDiv100Table) <- c("tuples", "dataset", "Strategy")
+colnames(watDivMoreTable) <- c("tuples", "endpoints", "Strategy")
+colnames(watDivMorePllTable) <- c("tuples", "endpoints", "Strategy")
 colnames(tuplesTable) <- c("tuples", "dataset", "Strategy")
 
 # create the boxplots
@@ -278,25 +287,18 @@ pdf("../results/watDiv/transferred_tuples.pdf", width=7, height=4)
 bootstrap_ggplot(ggplot(data = watDivTable, aes(x=Strategy, y=tuples)), strategyLabel, tuplesLabel)
 dev.off()
 
-pdf("../results/watDiv/transferred_tuples_20endpoints.pdf", width=7, height=4)
-bootstrap_ggplot(ggplot(data = watDiv20eTable, aes(x=Strategy, y=tuples)), strategyLabel, tuplesLabel)
-dev.off()
-
-pdf("../results/watDiv/transferred_tuples_30endpoints.pdf", width=7, height=4)
-bootstrap_ggplot(ggplot(data = watDiv30eTable, aes(x=Strategy, y=tuples)), strategyLabel, tuplesLabel)
-dev.off()
-
-pdf("../results/watDiv/parallelized/transferred_tuples_20endpoints.pdf", width=7, height=4)
-bootstrap_ggplot(ggplot(data = watDivPll20eTable, aes(x=Strategy, y=tuples)), strategyLabel, tuplesLabel)
-dev.off()
-
-pdf("../results/watDiv/parallelized/transferred_tuples_30endpoints.pdf", width=7, height=4)
-bootstrap_ggplot(ggplot(data = watDivPll30eTable, aes(x=Strategy, y=tuples)), strategyLabel, tuplesLabel)
-dev.off()
-
 pdf("../results/watDiv100/transferred_tuples.pdf", width=7, height=4)
 bootstrap_ggplot(ggplot(data = watDiv100Table, aes(x=Strategy, y=tuples)), strategyLabel, tuplesLabel)
 dev.off()
+
+pdf("../results/watDivMore/transferred_tuples.pdf", width=7, height=4)
+bootstrap_ggplot(ggplot(data = watDivMoreTable, aes(x=endpoints, y=tuples)), endpointsLabel, tuplesLabel, aes(fill=Strategy))
+dev.off()
+
+pdf("../results/watDivMore/parallelized/transferred_tuples.pdf", width=7, height=4)
+bootstrap_ggplot(ggplot(data = watDivMorePllTable, aes(x=endpoints, y=tuples)), endpointsLabel, tuplesLabel, aes(fill=Strategy))
+dev.off()
+
 
 # For the completness
 # Process the datas & merge them into one unique table
@@ -305,17 +307,11 @@ linkedMDBTable <- processTable(linkedMDBSetupName, 6, outputLinkedMDBEngine, out
 geoCoordinatesTable <- processTable(geoCoordinatesSetupName, 6, outputGeoCoordinatesEngine, outputGeoCoordinatesFedra, outputGeoCoordinatesPBJPre, outputGeoCoordinatesPBJPost, outputGeoCoordinatesPBJHybrid)
 SWDFTable <- processTable(swdfSetupName, 6, outputSWDFEngine, outputSWDFFedra, outputSWDFPBJPre, outputSWDFPBJPost, outputSWDFPBJHybrid)
 watDivTable <- processTable(watDivSetupName, 6, outputWatDivEngine, outputWatDivFedra, outputWatDivPBJPre, outputWatDivPBJPost, outputWatDivPBJHybrid)
-watDiv20eTable <- processTable(watDivSetupName, 6, outputWatDiv20eEngine, outputWatDiv20eFedra, outputWatDiv20ePBJPre, outputWatDiv20ePBJPost, outputWatDiv20ePBJHybrid)
-watDivPll20eTable <- processTable(watDivSetupName, 6, outputWatDivPll20eEngine, outputWatDivPll20eFedra, outputWatDivPll20ePBJPre, outputWatDivPll20ePBJPost, outputWatDivPll20ePBJHybrid)
-#watDiv30eTable <- processTable(watDivSetupName, 6, outputWatDiv30eEngine, outputWatDiv30eFedra, outputWatDiv30ePBJPre, outputWatDiv30ePBJPost, outputWatDiv30ePBJHybrid)
-#watDivPll30eTable <- processTable(watDivSetupName, 6, outputWatDivPll30eEngine, outputWatDivPll30eFedra, outputWatDivPll30ePBJPre, outputWatDivPll30ePBJPost, outputWatDivPll30ePBJHybrid)
-
-# TEMP - remove when all the results are here
-watDiv30eTable <- processTable(watDivSetupName, 6, outputWatDivEngine, outputWatDiv30eFedra, outputWatDivPBJPre, outputWatDivPBJPost, outputWatDiv30ePBJHybrid)
-watDivPll30eTable <- processTable(watDivSetupName, 6, outputWatDivEngine, outputWatDivPll30eFedra, outputWatDivPBJPre, outputWatDivPBJPost, outputWatDivPll30ePBJHybrid)
-# END TEMP
-
 watDiv100Table <- processTable(watDiv100SetupName, 6, outputWatDiv100Engine, outputWatDiv100Fedra, outputWatDiv100PBJPre, outputWatDiv100PBJPost, outputWatDiv100PBJHybrid)
+
+watDivMoreTable <- processFederationTables(6, outputWatDivFedra, outputWatDivPBJHybrid, outputWatDiv20eFedra, outputWatDiv20ePBJHybrid, outputWatDiv30eFedra, outputWatDiv30ePBJHybrid)
+watDivMorePllTable <- processFederationTables(6, outputWatDivFedra, outputWatDivPBJHybrid, outputWatDivPll20eFedra, outputWatDivPll20ePBJHybrid, outputWatDivPll30eFedra, outputWatDivPll30ePBJHybrid)
+
 completnessTable <- rbind(diseasomeTable, linkedMDBTable, geoCoordinatesTable, SWDFTable, watDivTable, watDiv100Table)
 
 # set the colnames
@@ -324,11 +320,9 @@ colnames(linkedMDBTable) <- c("completness", "dataset", "Strategy")
 colnames(geoCoordinatesTable) <- c("completness", "dataset", "Strategy")
 colnames(SWDFTable) <- c("completness", "dataset", "Strategy")
 colnames(watDivTable) <- c("completness", "dataset", "Strategy")
-colnames(watDiv20eTable) <- c("completness", "dataset", "Strategy")
-colnames(watDiv30eTable) <- c("completness", "dataset", "Strategy")
-colnames(watDivPll20eTable) <- c("completness", "dataset", "Strategy")
-colnames(watDivPll30eTable) <- c("completness", "dataset", "Strategy")
 colnames(watDiv100Table) <- c("completness", "dataset", "Strategy")
+colnames(watDivMoreTable) <- c("completness", "endpoints", "Strategy")
+colnames(watDivMorePllTable) <- c("completness", "endpoints", "Strategy")
 colnames(completnessTable) <- c("completness", "dataset", "Strategy")
 
 # create the boxplots
@@ -356,22 +350,14 @@ pdf("../results/watDiv/completeness.pdf", width=7, height=4)
 bootstrap_ggplot(ggplot(data = watDivTable, aes(x=Strategy, y=completness)), strategyLabel, completenessLabel, NULL, TRUE)
 dev.off()
 
-pdf("../results/watDiv/completeness_20endpoints.pdf", width=7, height=4)
-bootstrap_ggplot(ggplot(data = watDiv20eTable, aes(x=Strategy, y=completness)), strategyLabel, completenessLabel, NULL, TRUE)
-dev.off()
-
-pdf("../results/watDiv/completeness_30endpoints.pdf", width=7, height=4)
-bootstrap_ggplot(ggplot(data = watDiv30eTable, aes(x=Strategy, y=completness)), strategyLabel, completenessLabel, NULL, TRUE)
-dev.off()
-
-pdf("../results/watDiv/parallelized/completeness_20endpoints.pdf", width=7, height=4)
-bootstrap_ggplot(ggplot(data = watDivPll20eTable, aes(x=Strategy, y=completness)), strategyLabel, completenessLabel, NULL, TRUE)
-dev.off()
-
-pdf("../results/watDiv/parallelized/completeness_30endpoints.pdf", width=7, height=4)
-bootstrap_ggplot(ggplot(data = watDivPll30eTable, aes(x=Strategy, y=completness)), strategyLabel, completenessLabel, NULL, TRUE)
-dev.off()
-
 pdf("../results/watDiv100/completeness.pdf", width=7, height=4)
 bootstrap_ggplot(ggplot(data = watDiv100Table, aes(x=Strategy, y=completness)), strategyLabel, completenessLabel, NULL, TRUE)
+dev.off()
+
+pdf("../results/watDivMore/completeness.pdf", width=7, height=4)
+bootstrap_ggplot(ggplot(data = watDivMoreTable, aes(x=endpoints, y=completness)), endpointsLabel, completenessLabel, aes(fill=Strategy), TRUE)
+dev.off()
+
+pdf("../results/watDivMore/parallelized/completeness.pdf", width=7, height=4)
+bootstrap_ggplot(ggplot(data = watDivMorePllTable, aes(x=endpoints, y=completness)), endpointsLabel, completenessLabel, aes(fill=Strategy), TRUE)
 dev.off()
